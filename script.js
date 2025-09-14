98715ac9f52c2f2e8a171857be07d79bfa0fc28c
M00RTEN <yamorten@gmail.com>
2025-09-14 16:11:36 +0200

    Add files via upload
    Endringer i komplimenter.

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const compliments = [
   "Du ser nydlig ut idag♡",
-  "Har noen sagt hvor bra person du er? Eller må jeg gjøre det igjen?",
-  "Du ser litt søt ut når du prøver skjule at du scanner denne QR koden.",
-  "Din positivitet er inspirerende.",
-  "Du er flink til å få andre til å føle seg bra.",
+  "Har noen sagt hvor bra person du er? For det er du♡",
+  "Du ser litt søt ut når du prøver skjule at du scanner denne QR koden ♡",
+  "Din positivitet er inspirerende♡",
+  "Du er flink til å få andre til å føle seg bra♡",
   "Du er unik ♡",
-  "Psykisk helse er viktig, så lite kan endre en hel dag for en person.",
-  "Du stråler idag ♡."
+  "Et hei fra deg kan være nok for og gi en person mer håp♡",
+  "Du er godtnok ♡",
+  "Du stråler idag ♡"
+  "M00RTEN på sosiale medier, refresh nettsiden for positivitet♡"
 ];
 
 function getRandomCompliment() {
